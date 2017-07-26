339868cb7a55a778d616f0a12b0daf7d28758eaa
Pedro Alejandro Avila Plaza <pedro.avila@globant.com>
2017-07-26 11:58:06 -0500

    adding webpack image loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,45 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  devtool: 'source-map',
-  entry: {
-    app: [
-      'babel-polyfill',
-      path.join(__dirname, 'client', 'app/app.js'),
-    ],
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'ng-annotate-loader!babel-loader' },
-      { test: /\.html$/, loader: 'raw-loader' },
-      { test: /\.(scss|sass)$/, loader: 'style-loader!css-loader!sass-loader' },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
-    ],
-  },
-  plugins: [
-    // Injects bundles in your index.html instead of wiring all manually.
-    // It also adds hash to all injected assets so we don't have problems
-    // with cache purging during deployment.
-    new HtmlWebpackPlugin({
-      template: 'client/index.html',
-      inject: 'body',
-      hash: true,
-      chunks: ['vendor', 'app'],
-    }),
+    devtool: 'source-map',
+    entry: {
+        app: [
+            'babel-polyfill',
+            path.join(__dirname, 'client', 'app/app.js'),
+        ],
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'ng-annotate-loader!babel-loader' },
+            { test: /\.html$/, loader: 'raw-loader' },
+            { test: /\.(scss|sass)$/, loader: 'style-loader!css-loader!sass-loader' },
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file?hash=sha512&digest=hex&name=assets/img/[name].[ext]',
+                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+                ]
+            }
+        ],
+    },
+    plugins: [
+        // Injects bundles in your index.html instead of wiring all manually.
+        // It also adds hash to all injected assets so we don't have problems
+        // with cache purging during deployment.
+        new HtmlWebpackPlugin({
+            template: 'client/index.html',
+            inject: 'body',
+            hash: true,
+            chunks: ['vendor', 'app'],
+        }),
 
-    // Automatically move all modules defined outside of application directory to vendor bundle.
-    // If you are using more complicated project structure,
-    // consider to specify common chunks manually.
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: module => /node_modules/.test(module.resource),
-    }),
-  ],
+        // Automatically move all modules defined outside of application directory to vendor bundle.
+        // If you are using more complicated project structure,
+        // consider to specify common chunks manually.
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: module => /node_modules/.test(module.resource),
+        }),
+    ],
 };
